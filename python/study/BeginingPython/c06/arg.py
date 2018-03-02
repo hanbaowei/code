e287ed0177f01f91a3869281866d13c838139c69
hanbaowei <withon@yeah.net>
2018-03-02 09:39:48 +0800

    20180301

---
+++ python/study/BeginingPython/c06/arg.py
@@ -0,0 +1,18 @@
+
+def change(n):
+    n = 'ab'
+
+
+name = 'hanbaowei'
+change(name)
+print name
+
+foo = 1
+change(foo)
+print foo
+
+def g(x):
+    return x.isalnum()
+
+print g('10')
+print g('a')
